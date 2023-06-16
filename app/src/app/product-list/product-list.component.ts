b690ec9b8f721131a52b8ce0fe7f5306470a7dac
Marley <marley.nodejs@gmail.com>
2023-06-16 16:04:47 +0300

    #17 Event Binding | Angular Components & Directives | A Complete Angular Course

--- app/src/app/product-list/product-list.component.ts
+++ app/src/app/product-list/product-list.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent {
-  name = 'John Doe';
+  // name = 'John Doe';
+  addToCart: number = 0;
   product = {
     name: 'iPhone',
     price: 799,
@@ -20,5 +21,21 @@ export class ProductListComponent {
     return (
       this.product.price - (this.product.price * this.product.discount) / 100
     );
+  }
+
+  onNameChange(event: any) {
+    //this.name = event.target.value;
+  }
+
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue() {
+    if (this.addToCart < this.product.inStock) {
+      this.addToCart++;
+    }
   }
 }
